98356f2bccffba5845724f50cd0de65d43b73066
trupchan <trupka611@gmail.com>
2025-09-02 20:47:14 +0600

    message

---
+++ lesson1.py
@@ -0,0 +1,49 @@
+# HeroMag
+# hero_kirito
+
+
+
+class Hero:
+
+    #конструктор класса
+    def __init__(self, name, lvl, hp):
+
+        #атрибуты класса
+        self.name_1 = name
+        self.lvl_1 = lvl
+        self.hp_1 = hp
+
+
+
+        #метод класса
+    def action(self):
+        print("Base action")
+
+
+            #обьект.экземпляо класса
+kirito = Hero("kirito", 100, 1000)
+asuna = Hero("asuna", 100, 1000)
+
+text_1 = "text1"
+text_2 = "text2"
+
+print(kirito.name_1)
+print(asuna.name_1)
+print(text_1)
+print(text_2)
+
+#print(kirito.hp_1)
+#kirito.action()
+
+#print(type(kirito))
+#print(type("str"))
+
+
+
+
+
+
+
+
+
+
